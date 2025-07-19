42f63f6993ca951875759896f558efb1fc7f4a39
sanjay-rb <sanjaybabu618@gmail.com>
2025-07-20 01:33:25 +0530

    Refactor quote generation function for improved error handling and clarity

--- quote_generator.py
+++ quote_generator.py
@@ -2,25 +2,30 @@
 import json
 import requests
 
-from const import FILE_QUOTE_TODAY
+from const import DEFAULT_QUOTE, FILE_QUOTE_TODAY
 
 
-def get_todays_quote():
-    response = requests.get("https://zenquotes.io/api/today")
-    response.raise_for_status()
-    data = response.json()
+def generate_quote():
+    try:
+        print(f"Generating quote for today...")
+        response = requests.get("https://zenquotes.io/api/today")
+        response.raise_for_status()
+        data = response.json()
 
-    if isinstance(data, list) and data:
-        with open(FILE_QUOTE_TODAY, "w") as f:
-            json.dump(data[0], f, indent=4)
-        return data[0]
-    else:
-        raise ValueError("Failed to fetch quote from API.")
+        if isinstance(data, list) and data:
+            with open(FILE_QUOTE_TODAY, "w") as f:
+                json.dump(data[0], f, indent=4)
+            return data[0]
+        else:
+            raise ValueError("Failed to fetch quote from API.")
+    except Exception as e:
+        print(f"Error generating quote: {e}")
+        return DEFAULT_QUOTE
 
 
 def main():
-    quote = get_todays_quote()
-    print(f"Today's quote: {quote}")
+    quote = generate_quote()
+    print(f"Generated quote: {quote}")
     return quote
 
 
